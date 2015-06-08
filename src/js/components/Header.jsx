0a3d1ac4f27e16d0a4b83e63835024ab2b1596c7
Jamie Rolfs <jamie.rolfs@gmail.com>
2015-06-08 15:12:50 -0700

    Add hasValue state to Header

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,23 +1,37 @@
 
-import React from 'react';
+import React      from 'react';
+import autobind   from 'autobind-decorator';
+import classNames from 'classnames';
 
 import Input from './Input.jsx';
 import Icon  from './Icon.jsx';
 
 
-class Footer extends React.Component {
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { hasValue: false };
+  }
+
   render() {
     return (
-      <header className="header-view">
+      <header className={classNames('header-view',  { 'has-value': this.state.hasValue })}>
         <Icon className="check" name="icon-check" />
-        <Input />
+        <Input onChange={this.onChange} />
         <button className="add-todo icon">
           <Icon name="icon-plus" />
         </button>
       </header>
     );
   }
+
+  @autobind
+  onChange(value) {
+    this.setState({ hasValue: !!value });
+  }
 }
 
 
-export default Footer;
+export default Header;
